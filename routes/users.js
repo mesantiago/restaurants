42f7b82c1b99bb63fca906749d0759c6d761f33c
mesantiago <mark@centricitgroup.com>
2020-05-15 16:32:04 +0800

    User Module

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
+var User = require('../schemas/user');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST user creation. */
+router.post('/', function(req, res, next) {
+  User.create(req.body)
+    .then(function({ id, email, password }) {
+      res.json({ id, email });
+    })
+    .catch(next);
+});
+
+/* POST user login. */
+router.post('/login', function(req, res, next) {
+  var { email, password } = req.body;
+  User.findOne({ email })
+    .then(function(user) {
+      user.comparePassword(password, function(error, isMatch) {
+        if (isMatch) {
+
+          res.json({
+            id: user.id
+          });
+        } else {
+          res.status(401).json({
+            message: 'Unauthenticated'
+          });
+        }
+      });
+    })
+    .catch(function(error) {
+      res.status(404).json({
+        message: 'User Not found'
+      })
+    });
 });
 
 module.exports = router;
